7a6d91903c61a3b7eb890ab60d0fcacff57fe2ae
Claire-Alice Hebert <clairealice@MacBook-Pro.local>
2020-07-24 17:04:21 -0700

    make package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='psf-weather-station',
+    version='0.1',
+    description='generate realistic atmospheric input parameters for point-spread function simulations',
+    url='http://github.com/cahebert/psf-weather-station',
+    author='claire',
+    author_email='chebert@stanford.edu',
+    license='MIT',
+    packages=['psf-weather-station'],
+    python_requires='>=3.5',
+    zip_safe=False
+)
