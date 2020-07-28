cd75f1085ccce4dc6917f0e06e1c068848b87ea4
Claire-Alice Hebert <clairealice@MacBook-Pro.local>
2020-07-27 17:38:50 -0700

    add matching and interpolating functions

--- pws/utils.py
+++ pws/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.interpolate import interp1d
 
 def process_telemetry(telemetry_df):
     '''
@@ -8,16 +9,17 @@ def process_telemetry(telemetry_df):
     - a dict holding a dataframe each for wind directions and speeds
     - a dict with masks for each of the above dfs
     '''
-    cp_wind_dir = pd.DataFrame(telemetry_df['WindDir_twr']) 
-    cp_wind_speed = pd.DataFrame(telemetry_df['WindSpd_twr'])
+    tel_dir = pd.DataFrame(telemetry_df['WindDir_twr']) 
+    tel_speed = pd.DataFrame(telemetry_df['WindSpd_twr'])
 
-    for dframe in [cp_wind_dir, cp_wind_speed]:
+    for dframe in [tel_dir, tel_speed]:
         dframe.index = pd.to_datetime(dframe['dts'], utc=True)
 
-    cp_masks = {'speed': cp_wind_speed != 0}
+    nonzero = tel_speed.index[tel_speed['vals'] != 0]
+    cut = tel_speed.index[tel_speed['vals'] < 40]
+    cp_masks = {'speed': nonzero & cut, 'dir': tel_dir.sample(n=50_000).index}
 
-    return {'dir': cp_wind_dir, 'speed': cp_wind_speed}, cp_masks
-
+    return {'dir': tel_dir, 'speed': tel_speed}, cp_masks
 
 def process_gfs(gfs_df):
     '''
@@ -35,6 +37,29 @@ def process_gfs(gfs_df):
     gfs_df['dir'] = [smooth_direction(gfs_df['v'].values[i], gfs_df['u'].values[i]) 
                      for i in range(len(gfs_df))]
     return gfs_df
+
+def match_telemetry(speed, direction, gfs_dates):
+    '''
+    return matched speed/direction data associated with the gfs dates 
+    specifically, return the median telemetry values in a +/- 30min interval around each GFS point
+    '''
+    speed_ids = [speed.index[abs(gfs_dates[i] - speed.index) < pd.Timedelta('30min')] 
+               for i in range(n_gfs)]
+    dir_ids = [direction.index[abs(gfs_dates[i] - direction.index) < pd.Timedelta('30min')] 
+               for i in range(n_gfs)]
+    
+    ids_to_keep = [i for i in range(n_gfs) if len(speed_ids[i])!=0 and len(dir_ids)!=0]
+    
+    matched_s = [np.median(speed.loc[speed_ids[i]]['vals']) for i in range(n_gfs) if i in ids_to_keep]
+    matched_d = [np.median(direction.loc[dir_ids[i]]['vals']) for i in range(n_gfs) if i in ids_to_keep]
+
+    return matched_s, matched_d
+
+def interpolate(x, y):
+    new_x = np.linspace(min(x), max(x), 100)
+    f_y = interp1d(x, y, kind='cubic')
+    new_y = f_y(new_x)
+    return new_y
 
 def smooth_direction(V,U):
     '''
