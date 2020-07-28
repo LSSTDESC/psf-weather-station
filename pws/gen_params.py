8b5f800c53efeca4917ee0772f147df178ac75be
Claire-Alice Hebert <clairealice@MacBook-Pro.local>
2020-07-27 17:38:14 -0700

    improve loading and matching

--- pws/gen_params.py
+++ pws/gen_params.py
@@ -9,73 +9,72 @@ class ParameterGenerator():
     class object to generate realistic atmospheric input parameters required for GalSim simulation.
     uses NOAA GFS predictions matched with telemetry from Cerro Pachon.
     '''
-    def __init__(self):
-
-        # False until data have been temporally matched, to keep track of what is being used
-        self.matched = False 
+    def __init__(self, gfs_file='data/gfswinds_cp_20190501-20191031.pkl', gfs_h_file='data/H.npy',
+                 telemetry_file='data/cptelemetry_20190501-20191101.pkl'):
         # define path using pathlib
         self.p = pathlib.Path('.').resolve()
-        self.gfs_loaded = False
-        self.cp_loaded = False
+        
+        # check that the pointed data files exist:
+        self.gfs_f = self.p / pathlib.Path(gfs_file)
+        if not self.gfs_f.is_file():
+            raise FileNotFoundError(f'file {self.gfs_f} not found!')
 
-        self.gfs_ground = 6
+        self.gfs_h_f = self.p / pathlib.Path(gfs_h_file)
+        if not self.gfs_h_f.is_file():
+            raise FileNotFoundError(f'file {self.gfs_h_f} not found!')
 
-    def _load_gfs(self, gfs_path='data/gfswinds_cp_20190501-20191031.pkl'):
+        self.tel_f = self.p / pathlib.Path(telemetry_file)
+        if not self.tel_f.is_file():
+            raise FileNotFoundError(f'file {self.tel_f} not found!')
+
+        # altitude info
+        self.gfs_stop = 11
+        self.cp_ground = 2715 + 50
+
+    def _load_data(self):
         '''
         load pickle file of GFS winds and store in class
         '''
-        gfs_path = self.p / pathlib.Path(gfs_path)
+        gfs_winds = pickle.load(open(self.gfs_f, 'rb'))
+        gfs_winds = utils.process_gfs(gfs_winds)
 
-        if gfs_path.is_file():
-            gfs_winds = pickle.load(open(gfs_path, 'rb'))
-            self.gfs_winds = utils.process_gfs(gfs_winds)
-        else:
-            raise FileNotFoundError(f'file {gfs_path} not found!')
+        self.gfs_h = np.load(open(self.gfs_h_f, 'rb'))[::-1]
 
-        h_path = gfs_path.with_name('H.npy')
-        if h_path.is_file():
-            self.gfs_h = np.load(open(h_path, 'rb'))[::-1]
-        else:
-            raise FileNotFoundError(f'file {h_path} not found!')
+        telemetry = pickle.load(open(self.tel_f, 'rb'))
+        telemetry, tel_masks = utils.process_telemetry(telemetry)
 
-        return True
-
-    def _load_telemetry(self, telemetry_path='data/cptelemetry_20190501-20191101.pkl'):
-        '''
-        load pickle file of CP telemetry and store only winds in class
-        '''
-        telemetry_path = self.p / pathlib.Path(telemetry_path)
-
-        if telemetry_path.is_file():
-            telemetry = pickle.load(open(telemetry_path, 'rb'))
-
-            self.cp_telemetry, self.cp_masks = utils.process_telemetry(telemetry)
-            self.cp_ground = 2715 + 50
-        else:
-            raise FileNotFoundError(f'file {telemetry_path} not found!')
-
-        return True
+        return gfs_winds, telemetry, tel_masks
 
     def _match_data(self, dr=['2019-05-01', '2019-10-31']):
         '''
         temporally match the GFS and telemetry data
         '''
-        if self.gfs_loaded and self.cp_loaded:
-            print
-            gfs_dates = self.gfs_winds['speed'][dr[0]:dr[1]].index
-            n_gfs = len(gfs_dates)
-            cp_in_range = self.cp_telemetry['speed'][self.cp_masks['speed']][dr[0]:dr[1]]
-            
-            cp_matched = np.zeros(n_gfs)
-            for i in range(n_gfs):
-                cp_ids_close = abs(gfs_dates[i] - cp_in_range.index) < pd.Timedelta('30min')
-                cp_close = cp_in_range[cp_ids_close]['vals']
-                cp_matched[i] = cp_close[cp_close<40].median()
+        gfs_winds, telemetry, tel_masks= self._load_data()
 
-            self.cp_telemetry['matched'] = cp_matched
-            return True
-        else:
-            return False
+        gfs_dates = gfs_winds[dr[0]:dr[1]].index
+        n_gfs = len(gfs_dates)
+        
+        speed = telemetry['speed'].loc[tel_masks['speed']][dr[0]:dr[1]]
+        direction = telemetry['dir'].loc[tel_masks['dir']][dr[0]:dr[1]]
+
+        matched_s, matched_d = utils.match_telemetry(speed, directions, gfs_dates)
+        
+        self.telemetry = {'speed': matched_s,'dir': matched_d}
+        self.gfs_winds = gfs_winds.loc[gfs_dates].iloc[ids_to_keep]
+
+    def _construct_wind_set(self):
+        '''
+        construct a vector of winds vs altitude using GFS till self.gfs_stop and matched telemetry 
+        '''
+
+
+
+    def _interpolate_wind(self, h):
+        '''
+        use matched data to interpolate 
+        '''
+
+        # utils.interpolate()
 
     def _calculate_cn2(self):
         '''
--- tests/test_gen.py
+++ tests/test_gen.py
@@ -2,48 +2,76 @@ import numpy as np
 import pathlib 
 import pws
 
-def test_loading():
-    pgen = pws.gen_params.ParameterGenerator()
+## TO DO ##
+# test_matching does almost nothing except check that the method
+# didn't raise an error: make a better test.
 
-    try: 
-        gfs_l = pgen._load_gfs()
-        pgen.gfs_winds['speed']
+def test_init():
+    try:
+        pgen = pws.gen_params.ParameterGenerator()
+        return True
     except:
-        gfs_l = False
+        return False
 
-    try: 
-        cp_l = pgen._load_telemetry()
-        pgen.cp_telemetry['speed']
-    except:
-        cp_l = False
-
-    return gfs_l and cp_l
-
-def test_processing():
+def test_load():
     pgen = pws.gen_params.ParameterGenerator()
-    pgen._load_gfs()
-    t_speed = (pgen.gfs_winds['speed'].iloc[0] == np.hypot(pgen.gfs_winds['u'].iloc[0], 
-                                                     pgen.gfs_winds['v'].iloc[0])).all()
-    t_order = pgen.gfs_winds['speed'].iloc[0][10] > pgen.gfs_winds['speed'].iloc[0][0]
+    try: 
+        outs = pgen._load_data()
+        return len(outs)==3
+    except:
+        return False
+
+def test_gfs_processing():
+    pgen = pws.gen_params.ParameterGenerator()
+    gfs_winds, *rest = pgen._load_data()
+    t_speed = (gfs_winds['speed'].iloc[0] == np.hypot(gfs_winds['u'].iloc[0], 
+                                                      gfs_winds['v'].iloc[0])).all()
+    t_order = gfs_winds['speed'].iloc[0][10] > gfs_winds['speed'].iloc[0][0]
     return t_speed and t_order
+
+def test_telemetry_processing():
+    pgen = pws.gen_params.ParameterGenerator()
+    _, cp, mask = pgen._load_data()
+
+    try:
+        len(cp['dir']) > 100
+        len(cp['speed']) > 100
+    except:
+        return False
+
+    masked_speed = cp['speed'].loc[mask['speed']]['vals']
+    t_mask_cut = (np.max(masked_speed) < 40) 
+    t_mask_nonzero = (np.min(masked_speed) > 0)
+
+    return t_mask_cut and t_mask_nonzero
 
 def test_matching():
     pgen = pws.gen_params.ParameterGenerator()
-    pgen.gfs_loaded = pgen._load_gfs()
-    pgen.cp_loaded = pgen._load_telemetry()
 
+    # test lengths of matched data are nonzero 
+    # and that they match the gfs data!
+    # test that none of the matched data are nan
     try: 
-        matched = pgen._match_data()
-        pgen.cp_telemetry['matched']
+        pgen._match_data()
     except:
-        matched = False
+        return False
 
-    return matched
+    l_gfs = len(pgen.gfs_winds)
+    t_len = (len(pgen.telemetry['speed'])==l_gfs) and (len(pgen.telemetry['dir'])==l_gfs)
+
+    t_nan = np.isfinite(pgen.telemetry['speed']).all() and np.isfinite(pgen.dir['speed']).all()
+           
+    return t_len and t_nan
+
 
 if __name__ == '__main__':
     
-    assert test_loading(), "loading didn't work!"
+    assert test_init(), "initialization didn't work!"
 
-    assert test_processing(), "processing didn't work!"
+    assert test_load(), "loading didn't work!"
+
+    assert test_gfs_processing(), "gfs processing didn't work!"
+
+    assert test_telemetry_processing(), "telemetry processing didn't work!"
 
     assert test_matching(), "matching didn't work!"
