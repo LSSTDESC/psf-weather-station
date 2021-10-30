cb3feb5f714027ba6136f20ce2bf85cc26e41972
Claire-Alice Hebert <claireahebert@me.com>
2021-10-29 18:23:31 -0700

    fix install?

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='psfws',
     version='0.1',
     description='generate realistic wind and turbulence parameters for atmospheric point-spread function simulations',
-    url='http://github.com/cahebert/psf-weather-station',
+    url='https://github.com/LSSTDESC/psf-weather-station',
     author='Claire-Alice Hebert',
     author_email='chebert@stanford.edu',
     license='MIT',
-    # packages=['psfws'],
+    packages=['psfws'],
     python_requires='>=3.5'
 )
